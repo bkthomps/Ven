d8fc40fb73731b3de620c22f761451828829b496
bkthomps <bkthomps@uwaterloo.ca>
2020-11-01 17:23:15 -0500

    Refactor

--- ven.go
+++ ven.go
@@ -1,33 +1,12 @@
-/*
-Copyright (c) 2019 Bailey Thompson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/bkthomps/Ven/screen"
 	"github.com/gdamore/tcell"
 	"github.com/gdamore/tcell/encoding"
-	"log"
-	"os"
 )
 
 const version = "0.3.0"
@@ -44,13 +23,14 @@ func main() {
 		print("Available at github.com/bkthomps/Ven\n")
 		return
 	}
-	s, e := tcell.NewScreen()
-	if e != nil {
-		log.Fatal(e)
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatal(err)
 	}
 	encoding.Register()
 	quit := make(chan struct{})
-	screen.Init(s, quit, userArg)
+	state := &screen.State{}
+	state.Init(s, quit, userArg)
 	<-quit
 	s.Fini()
 }
